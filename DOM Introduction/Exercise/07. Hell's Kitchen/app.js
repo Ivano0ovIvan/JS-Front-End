ffb0a10d7d1578cee465b7025f19e1b532250786
Ivano0ovIvan <i_ivano0ov@abv.bg>
2023-11-30 12:42:56 +0200

    ...

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -1,18 +1,18 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
+   function onClick() {
       let inputArray = JSON.parse(document.querySelector('#inputs textarea').value);
-      
+
       let restaurantsMap = {};
 
       inputArray.forEach(entry => {
          const [restaurantName, data] = entry.split(' - ');
          const workerData = data.split(', ');
-       
+
          const workers = workerData.map(worker => {
-           const [name, salary] = worker.split(' ');
-           return { name, salary: parseInt(salary) };
+            const [name, salary] = worker.split(' ');
+            return { name, salary: parseInt(salary) };
          });
 
          if (restaurantsMap[restaurantName]) {
@@ -22,32 +22,39 @@ function solve() {
                name: restaurantName,
                workers: workers.sort((a, b) => b.salary - a.salary),
                bestSalary: 0,
-               averageSalary: 0
+               averageSalary: 0,
             };
          }
       });
-      
+
       let restaurantsArray = Object.values(restaurantsMap);
 
       for (const restaurant of restaurantsArray) {
-         console.log(Math.max(restaurant.workers.map(worker => worker.salary)));
-         //restaurant.bestSalary = Math.max(restaurant.workers.map(worker => worker.salary));
-         //restaurant.averageSalary = (restaurant.workers.reduce((acc, worker) => acc + worker.salary, 0) / workers.length).toFixed(2);
-      }
-      
-      let bestRestaurant = restaurantsArray[0];
 
-      restaurantsArray.forEach(restaurant => {
-         if (restaurant.averageSalary > bestRestaurant.averageSalary) {
-            bestRestaurant = restaurant;
+         let totalSalary = 0;
+
+         for (const worker of restaurant.workers) {
+            totalSalary += worker.salary
+
+            if (worker.salary > restaurant.bestSalary) {
+               restaurant.bestSalary = worker.salary
+            }
          }
-      }) 
+
+         restaurant.averageSalary = (totalSalary / restaurant.workers.length).toFixed(2);
+
+      }
+
+      let bestRestaurant = restaurantsArray.reduce((prev, current) => {
+         return parseFloat(prev.averageSalary) > parseFloat(current.averageSalary) ? prev : current;
+      }, {});
 
       let bestRestaurantResult = `Name: ${bestRestaurant.name} Average Salary: ${bestRestaurant.averageSalary} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`;
-      let bestRestaurantWorkers = ''
+      let bestRestaurantWorkers = '';
+
       for (const worker of bestRestaurant.workers) {
          bestRestaurantWorkers += `Name: ${worker.name} With Salary: ${worker.salary} `
-      }
+      };
 
       document.querySelector('#bestRestaurant p').textContent = bestRestaurantResult;
       document.querySelector('#workers p').textContent = bestRestaurantWorkers;
